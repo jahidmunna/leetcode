89ad2de09a4a1e769646c66724c55b5a091d2300
jahidmunna <jimunna150@gmail.com>
2022-08-07 00:24:55 +0900

    added linked list - easy

---
+++ reverse-linked-list.py
@@ -0,0 +1,30 @@
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+
+# Input: head = [1,2]
+# Output: [2,1]
+
+# Input: head = []
+# Output: []
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        prev = None
+        curr = head
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
