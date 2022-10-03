d16197c7764e53bad463999768991bdec9c4c449
Munna, Jahidulislam | Jahid | RMI <jahidulislam.munna@rakuten.com>
2022-10-03 10:50:42 +0900

    Improvise the time complexity

--- find_first_and_last_index.py
+++ find_first_and_last_index.py
@@ -16,29 +16,50 @@
 # Output: [-1,-1]
 
 from typing import List
-# from collections import defaultdict
-
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1, -1]
+        
         result = []
-        for i, num in enumerate(nums,0):
-            if num == target:
-                result.append(i)
-        
-        if len(result)>1:
-            return [result[0], result[-1]]
-        
-        if len(result) == 1:
-            return [result[0], result[0]]
-        
-        return [-1, -1]
+        low, high = 0, len(nums) -1
+        while low<=high:
+            mid = int((low+high)/2)
+            current = nums[mid]
+            if current == target:
+                result.append(mid)
+                i = mid-1 
+                j = mid +1
+                size = len(nums) 
+                while i>=0 or j<size:
+                    if i>=0:
+                        prev = nums[i]
+                        if prev == target:
+                            result.append(i)
+                            i -=1
+                        else: 
+                            i = -1
+                    if j<size:
+                        next = nums[j]
+                        if next == target:
+                            result.append(j)
+                            j +=1
+                        else:
+                            j = len(nums) 
+            
+                break 
+            elif current < target:
+                low = mid+1 
+            else:
+                high = mid -1
+        return [min(result), max(result)] if result else [-1, -1]
 
 
 obj = Solution()
 # result = obj.searchRange(nums = [5,7,7,8,8,10], target = 8)
-result = obj.searchRange(nums = [5,7,7,8,8,10], target = 6)
-
-
-
+# result = obj.searchRange(nums = [5,7,7,8,8,10], target = 6)
+# result = obj.searchRange(nums = [3, 3, 3], target = 3)
+# result = obj.searchRange(nums = [1], target = 1)
+result = obj.searchRange(nums = [2, 2], target = 2)
 
 print(result)
